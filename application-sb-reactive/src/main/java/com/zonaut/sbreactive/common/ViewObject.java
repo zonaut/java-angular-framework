ae186a7832a80bb4c233ff1f9c28aa935aa7df91
zonaut <info@zonaut.com>
2022-03-07 16:27:31 +0100

    update

--- application-sb-reactive/src/main/java/com/zonaut/sbreactive/common/ViewObject.java
+++ application-sb-reactive/src/main/java/com/zonaut/sbreactive/common/ViewObject.java
@@ -1,26 +1,7 @@
 package com.zonaut.sbreactive.common;
 
 import java.io.Serializable;
-//import javax.persistence.MappedSuperclass;
-//
-//import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
-//import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-//import org.hibernate.annotations.TypeDef;
-//import org.hibernate.annotations.TypeDefs;
-//
-//@TypeDefs({
-//    @TypeDef(name = ViewObject.TYPE_ENUM, typeClass = PostgreSQLEnumType.class),
-//    @TypeDef(name = ViewObject.TYPE_JSONB, typeClass = JsonBinaryType.class)
-//})
-//@MappedSuperclass
+
 public class ViewObject implements Serializable {
-
-    public static final String TYPE_ENUM = "pgsql_enum";
-    public final static String TYPE_JSONB = "jsonb";
-
-    public static final String DEFINITION_INTEGER = "INTEGER";
-    public static final String DEFINITION_TEXT = "TEXT";
-    public static final String DEFINITION_UUID = "UUID";
-    public static final String DEFINITION_JSON_BINARY = "JSONB";
 
 }
